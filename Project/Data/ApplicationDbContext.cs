c7866b7dd49c3f5eb4a442c5b9d22a113a545027
pc11 <pc11@DESKTOP-58VGM0V>
2024-10-17 19:25:46 +0300

    Добавьте файлы проекта.

---
+++ Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Project.Data
+{
+    public class ApplicationDbContext
+    {
+    }
+}
---
+++ Project/Servises/IOrderServises.cs
@@ -0,0 +1,6 @@
+﻿namespace Project.Servises
+{
+    public interface IOrderServises
+    {
+    }
+}
